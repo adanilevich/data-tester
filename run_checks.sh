18003c5c3f1850a4b8acd992949ebd3aaf0994e5
Anatoly Danilevich <anatoly.danilevich@gmail.com>
2025-07-16 18:44:04 +0200

    .

--- run_checks.sh
+++ run_checks.sh
@@ -2,11 +2,11 @@
 
 echo "Running ruff checks..."
 ruff check --fix
-read -p "Press Enter to continue"
+read -p "Press Enter to continue!"
 
 echo "Running mypy checks..."
 mypy
-read -p "Press Enter to continue"
+read -p "Press Enter to continue!"
 
 echo "Running pytest..."
 pytest
